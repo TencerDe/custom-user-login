e355a97e7b0e1c13cdd508218997b3c232cae902
TencerDe <tsharma2907@gmail.com>
2025-01-30 22:52:08 +0530

    commit 2

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
 
 # Create your views here.
+
+def sign_up(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password  = request.POST.get("password")
+
+        if CustomUser.objects.filter(username=username).exists():
+            return render(request, 'sign_up.html', {'error': 'Username already exists.'})
+        
+        user = CustomUser(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        return redirect("log_in")
+    return render(request, 'sign_up.html')
+
+def log_in(request):
+           if request.method == "POST":
+                username = request.POST.get("username")
+                password = request.POST.get("password")
+
+                try:
+                      user = CustomUser.objects.get(username=username)
+                      if user.check_password(password):
+                            request.session["user_id"] = user.id
+                            return redirect("dashboard")
+                      else:
+                            return render(request, "log_in.html", {"error": "Invalid credentials"})
+                except CustomUser.DoesNotExist:
+                      return render(request, "log_in.html", {"error": "user not found"})
+                
+                return render(request,"log_in.html")
